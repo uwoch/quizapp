61d1e5b6ec560ffbde2f5887d0938ec42b78d9a1
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-04-06 13:02:37 +0200

    add changes

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const bodyElement = document.querySelector('[data-js="body"]');
-
 const toggleBookmark = document.querySelector('[data-js="toggle-bookmark"]');
 toggleBookmark.addEventListener("click", () => {
     toggleBookmark.classList.toggle("selected");
