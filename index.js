2770c085e81e269ecdba84f1de28b3c587c0c310
Ulrike Wochnig <ulrike.wochnig@gmail.com>
2023-04-06 09:53:17 +0200

    change bookmark

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const bodyElement = document.querySelector('[data-js="body"]');
+
 const toggleBookmark = document.querySelector('[data-js="toggle-bookmark"]');
 toggleBookmark.addEventListener("click", () => {
     toggleBookmark.classList.toggle("selected");
